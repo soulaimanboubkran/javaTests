34fb57439b730fddde0642fa6753acbcd95ba38d
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2024-12-04 19:57:10 +0100

    commit

--- algos/Array.java
+++ algos/Array.java
@@ -40,15 +40,47 @@ class Array {
         }
         return secondMax;
     }
-    public static void main(String[] args) {
-        int[] reversedArray = reverse(new int[]{2, 1, 3, 4, 5, 6, 7}); // Call reverse using the class name
-        System.out.println(min(reversedArray));
+    public static int[] moveZeros(int[] nums) {
+        int j = 0; // Pointer for the next zero position
 
-        System.out.println(secondMax(new int[]{7, 1, 3, 4, 5, 6, 7}));
-       for (int num : reversedArray) {
-            System.out.print(num + " ");
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                // Swap only if `i` is not equal to `j`
+                if (i != j) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+                j++; // Move `j` to the next position
+            }
         }
+        return nums;
+    }
+
+    public static boolean isPalindrome(String word){
+        char[] charArray = word.toCharArray();
+        int start = 0;
+        int end = word.length() -1;
+        while (start < end) {
+            if(charArray[start] != charArray[end]){
+                return  false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+      //  int[] reversedArray = reverse(new int[]{2, 1, 3, 0, 0, 6, 7}); // Call reverse using the class name
        
+      //  System.out.println(min(reversedArray));
+
+      //  System.out.println(secondMax(new int[]{7, 1, 3, 4, 5, 6, 7}));
+      
+    //  for (int num : moveZeros(new int[]{2, 1, 3, 0, 0, 6, 7})) {
+    //        System.out.print(num + " ");
+    //    }
+     // System.out.println(isPalindrome("")); 
     }
 }
 
