a04476b208a54381a3eb8e1a8799d83c9713c346
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2024-12-30 12:01:56 +0100

    commit

---
+++ tests/api2/controller/Controller2.java
@@ -0,0 +1,13 @@
+package tests.api2.controller;
+
+import java.util.Date;
+
+public class Controller2 {
+    public static long timeMs(){
+        return System.currentTimeMillis();
+    }
+    public static Date dateNow(){
+        return new Date();
+    }
+ 
+}
