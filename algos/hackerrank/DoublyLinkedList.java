f8c71f869d8854a15e652d09d598ef8367c147e5
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2025-02-13 02:11:18 +0100

    commit

--- algos/hackerrank/DoublyLinkedList.java
+++ algos/hackerrank/DoublyLinkedList.java
@@ -12,6 +12,30 @@ public class DoublyLinkedList {
             this.prev = null;
         }
     }
+
+    // just swap the next and prev pointers of each node // so when you swap in each node the next current one will be riched by the prev pointer
+    public static DoublyLinkedListNode reverse(DoublyLinkedListNode llist) {
+        if (llist == null) return null; // Handle empty list case
+        
+        DoublyLinkedListNode current = llist;
+        DoublyLinkedListNode newHead = null;
+
+        while (current != null) {
+            // Swap next and prev pointers
+            DoublyLinkedListNode temp = current.prev;
+            current.prev = current.next;
+            current.next = temp;
+
+            // Move newHead forward
+            newHead = current;
+
+            // Move to the next node (which is actually prev now)
+            current = current.prev;
+        }
+
+        return newHead; // New head is the last node of original list
+
+    }
     public static DoublyLinkedListNode sortedInsert(DoublyLinkedListNode llist, int data) {
         DoublyLinkedListNode newNode = new DoublyLinkedListNode(data);
 
