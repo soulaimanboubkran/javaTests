719881cf886c48df2241be0c69756de1289327b5
soulaiman bouiboukirn <soulaiman.soulb@gmail.com>
2025-02-18 23:28:27 +0100

    commit

---
+++ algos/hackerrank/Stacks.java
@@ -0,0 +1,25 @@
+package algos.hackerrank;
+
+import java.util.*;
+
+public class Stacks {
+    
+    public static boolean isValid(String s) {
+        Stack<Character>  stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty() ) return false;
+
+                char top = stack.pop();
+                if((c == ')' && top != '(') || 
+                (c == '}' && top != '{') || 
+                (c == ']' && top != '[')) {
+                return false;
+            }
+        }
+    }
+    return stack.isEmpty();
+}
+}
